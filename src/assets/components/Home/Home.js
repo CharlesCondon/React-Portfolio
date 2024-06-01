c1a3fe1008f82ec03f3db090f7cb6e4cfe4b60de
Charles <charlescon@live.com>
2024-06-01 18:44:56 -0500

    projects added to home with hover effects

--- src/assets/components/Home/Home.js
+++ src/assets/components/Home/Home.js
@@ -4,6 +4,9 @@ import sphere from '../../js/sphere';
 // import cube from '../../js/cube';
 import { ReactP5Wrapper } from '@p5-wrapper/react';
 import ProjectItem from './ProjectItem/ProjectItem';
+import talrd from '../../images/talrd.png';
+import auraVista from '../../images/auraVista.png';
+import plumbing from '../../images/plumbing.png'
 
 function Home() {
 
@@ -27,13 +30,13 @@ function Home() {
             <div className='projectsCont'>
                 <p>Works</p>
                 <div className='projectsList'>
-                    <ProjectItem  project={{ title:'CPC-Studio', image:{}, tags:['Website Design','Development', 'Animation', "UI/UX"], url:'' }}/>
-                    <ProjectItem  project={{ title:'Aura Vista', image:{}, tags:['Website Design','Development', "UI/UX"], url:'' }}/>
-                    <ProjectItem  project={{ title:'Boss Plumbing', image:{}, tags:['Website Design','UI/UX', 'Branding'], url:'' }}/>
-                    <ProjectItem  project={{ title:'TALRD', image:{}, tags:['Website Design','UI/UX', 'Branding'], url:'' }}/>
-                    <ProjectItem  project={{ title:'Sound Visualizer', image:{}, tags:['Animation','Development'], url:'' }}/>
-                    <ProjectItem  project={{ title:'Sound Visualizer 2', image:{}, tags:['Animation','Development'], url:'' }}/>
-                    <ProjectItem  project={{ title:'Wordle 2', image:{}, tags:['Website Design','Development'], url:'' }}/>
+                    <ProjectItem  project={{ title:'CPC-Studio', image:talrd, tags:['Website Design','Development', 'Animation', "UI/UX"], url:'' }}/>
+                    <ProjectItem  project={{ title:'Aura Vista', image:auraVista, tags:['Website Design','Development', "UI/UX"], url:'' }}/>
+                    <ProjectItem  project={{ title:'Boss Plumbing', image:plumbing, tags:['Website Design','UI/UX', 'Branding'], url:'' }}/>
+                    <ProjectItem  project={{ title:'TALRD', image:talrd, tags:['Website Design','UI/UX', 'Branding'], url:'' }}/>
+                    <ProjectItem  project={{ title:'Music Visualizer', image:talrd, tags:['Development','Animation'], url:'' }}/>
+                    <ProjectItem  project={{ title:'Music Visualizer 2', image:talrd, tags:['Development','Animation'], url:'' }}/>
+                    <ProjectItem  project={{ title:'Wordle 2', image:talrd, tags:['Website Design','Development'], url:'' }}/>
                 </div>
             </div>
         </div>
--- src/assets/components/Home/ProjectItem/ProjectItem.js
+++ src/assets/components/Home/ProjectItem/ProjectItem.js
@@ -2,21 +2,28 @@ import React from 'react';
 import './ProjectItem.css';
 
 function ProjectItem({project}) {
+    const [toggleImg, setToggleImg] = React.useState(false);
+
     function handleClick() {
 
     }
     function handleHover() {
-
+        if (window.innerWidth > 992) {
+            setToggleImg(!toggleImg)
+        } else {
+            setToggleImg(false)
+        }
     }
-
+    console.log(project)
     return (
-        <div className='projectItem' onClick={handleClick} onMouseEnter={handleHover}>
+        <div className='projectItem' onClick={handleClick} onMouseEnter={handleHover} onMouseLeave={handleHover}>
             <h2>{project.title}</h2>
             <div className='projectTagList'>
                 {project.tags.map((t) => {
                     return <p className='projectTag'>{t}</p>
                 })}
             </div>
+            {toggleImg && (<div className='projectImg'><img src={project.image} alt='' /></div>)}
         </div>
     )
 }
